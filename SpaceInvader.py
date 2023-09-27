0e7e4a098f72a9ac1744699d3e5788e135cb84de
Riwokib <riwellkib@gmail.com>
2023-09-27 23:37:47 +0300

    render score text, player spaceship and bg image

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -3,15 +3,27 @@ from sys import exit
 
 #INITIALIZE & SETUP
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invader")
-icon = pygame.image.load('spaceinvaders/spaceship.png').convert()
-pygame.display.set_icon(icon)
-clock = pygame.time.Clock()
+ICON = pygame.image.load('spaceinvaders/spaceship.png').convert()
+pygame.display.set_icon(ICON)
+SCORE_FONT = pygame.font.Font('freesansbold.ttf',32)
+GAME_OVER_FONT = pygame.font.Font('freesansbold.ttf',64)
+BUTTON_TEXT_FONT = pygame.font.Font('freesansbold.ttf',18)
+CLOCK = pygame.time.Clock()
 
+
+
+#SCORE
+score_value = 0
 
 #WORLD
-background = pygame.image.load("spaceinvaders/bg.jpg").convert()
+BG = pygame.image.load("spaceinvaders/bg.jpg").convert_alpha()
+score = SCORE_FONT.render('SCORE:' +str(score_value) , False, 'black')
+
+#PLAYER
+PLAYER = pygame.image.load('spaceinvaders/invader.png').convert_alpha()
+
 
 
 while True:
@@ -20,5 +32,15 @@ while True:
             pygame.quit()
             exit()
 
+    #SHOW BACKGROUND
+    SCREEN.blit(BG, (0,0))
+
+
+    #SHOW SCORE
+    SCREEN.blit(score, (10,10))
+
+    #SHOW PLAYER
+    SCREEN.blit(PLAYER, (400, 520))
+
     pygame.display.update()
-    clock.tick(60)
+    CLOCK.tick(60)
