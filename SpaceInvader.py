a4daeb23edc32f81c5b58b98f644ed46ade529d4
Riwokib <riwellkib@gmail.com>
2024-02-08 18:27:54 +0300

    sprite groups for collision btn enemy/player and enemy/bullet

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -10,17 +10,18 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invader")
 ICON = pygame.image.load('spaceinvaders/spaceship.png').convert()
 pygame.display.set_icon(ICON)
+game_active = True
 
 
 SCORE_FONT = pygame.font.Font('freesansbold.ttf',32)
 GAME_OVER_FONT = pygame.font.Font('freesansbold.ttf',64)
 BUTTON_TEXT_FONT = pygame.font.Font('freesansbold.ttf',18)
 
-#SCORE VALUE
-score_value = 0
-
 #WORLD
 BG = pygame.image.load("spaceinvaders/bg.jpg").convert()
+
+#SCORE VALUE
+score_value = 0
 score = SCORE_FONT.render('SCORE:' +str(score_value) , False, (64,64,64))
 score_rect = score.get_rect(bottomleft = (0,600))
 
@@ -29,23 +30,23 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,):
         super().__init__()
         self.PLAYER = pygame.image.load('spaceinvaders/invader.png').convert_alpha()
-        self.player_rect = self.PLAYER.get_rect(midbottom = (400,570))
-        self.player_velocity = 5
+        self.rect = self.PLAYER.get_rect(midbottom = (400,570))
+        self.player_velocity = 10
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
 
-        if pressed_keys[pygame.K_UP] and self.player_rect.top > 0:
-            self.player_rect.y -= self.player_velocity
-        if pressed_keys[pygame.K_DOWN] and self.player_rect.bottom < SCREEN_HEIGHT:
-            self.player_rect.y += self.player_velocity
-        if pressed_keys[pygame.K_LEFT] and self.player_rect.left > 0:
-            self.player_rect.x -= self.player_velocity
-        if pressed_keys[pygame.K_RIGHT] and self.player_rect.right < SCREEN_WIDTH:
-            self.player_rect.x += self.player_velocity
+        if pressed_keys[pygame.K_UP] and self.rect.top > 0:
+            self.rect.y -= self.player_velocity
+        if pressed_keys[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
+            self.rect.y += self.player_velocity
+        if pressed_keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.player_velocity
+        if pressed_keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+            self.rect.x += self.player_velocity
 
     def display(self, surface):
-        surface.blit(self.PLAYER, self.player_rect)
+        surface.blit(self.PLAYER, self.rect)
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -54,30 +55,26 @@ class Enemy(pygame.sprite.Sprite):
         self.ENEMY = pygame.image.load("spaceinvaders/enemy.png").convert_alpha()
         self.random_x_position = random.randint(0, SCREEN_WIDTH)
         #random_y_position = random.randint(-40, 0)
-        self.enemy_rect = self.ENEMY.get_rect(midtop = (self.random_x_position,0))
+        self.rect = self.ENEMY.get_rect(midtop = (self.random_x_position,0))
         self.enemy_speed = 0.01
         self.enemy_angle = 0
 
     def move(self):
-        self.enemy_rect.x += math.sin(self.enemy_angle) * self.enemy_speed
-        self.enemy_rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
+        self.rect.x += math.sin(self.enemy_angle) * self.enemy_speed
+        self.rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
 
-        if self.enemy_rect.right > SCREEN_WIDTH:
+        if self.rect.right > SCREEN_WIDTH:
             self.enemy_angle *= -1
-            self.enemy_rect.x += math.sin(self.enemy_angle) * self.enemy_speed
-            self.enemy_rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
+            self.rect.x += math.sin(self.enemy_angle) * self.enemy_speed
+            self.rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
 
-        if self.enemy_rect.left < 0:
+        if self.rect.left < 0:
             self.enemy_angle *= -1
-            self.enemy_rect.x += math.sin(self.enemy_angle) * self.enemy_speed
-            self.enemy_rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
-
-
-
-    
+            self.rect.x += math.sin(self.enemy_angle) * self.enemy_speed
+            self.rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
 
     def display(self, surface):
-        surface.blit(self.ENEMY, self.enemy_rect)
+        surface.blit(self.ENEMY, self.rect)
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -86,18 +83,20 @@ class Bullet(pygame.sprite.Sprite):
         self.BULLET = pygame.image.load('spaceinvaders/bullet.png').convert_alpha()
         bullet_x = player_rect.x + (player_rect.width // 2)
         bullet_y = player_rect.y + (player_rect.height // 2)
-        self.bullet_rect = self.BULLET.get_rect(midbottom =(bullet_x, bullet_y))
-        self.bullet_velocity = 1
+        self.rect = self.BULLET.get_rect(midbottom =(bullet_x + 5, bullet_y))
+        self.bullet_velocity = 15
 
     def fire_bullet(self):
-        self.bullet_rect.y -= self.bullet_velocity
+        self.rect.y -= self.bullet_velocity
 
     def display(self, surface):
-        surface.blit(self.BULLET, self.bullet_rect)
+        surface.blit(pygame.transform.scale(self.BULLET, (20,25)), self.rect)
 
 
-#CREATE PLAYER & ENEMY
+#CREATE THE PLAYER
 player = Player()
+
+#WAIT FOR ENEMIES & BULLETS
 bullets = []
 enemies = []
 
@@ -105,6 +104,23 @@ enemies = []
 SPAWN_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(SPAWN_ENEMY, 2000)
 
+
+#SPRITE GROUPS for collisions
+enemy_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+
+def collisions():
+    '''handle collitions between player & enemy
+        enemy & bullet'''
+    
+    if pygame.sprite.spritecollide(player, enemy_group, True):
+        print('enemy collision!!')
+    
+    for bullet in bullets:
+        if pygame.sprite.spritecollide(bullet, enemy_group, True):
+            print('enemy shot')
+            enemy_group.empty()
 
 
 def draw_window():
@@ -115,12 +131,11 @@ def draw_window():
     pygame.draw.rect(SCREEN, (255,182,193),score_rect)
     SCREEN.blit(score, score_rect)
 
-    
-
     #SHOW BULLETS
     for bullet in bullets:
         bullet.display(SCREEN)
         bullet.fire_bullet()
+        bullet_group.add(bullet)
     
     #SHOW & HANDLE PLAYER MOVEMENTS
     player.display(SCREEN)
@@ -130,10 +145,16 @@ def draw_window():
     for enemy in enemies:
         enemy.display(SCREEN)
         enemy.move()
+        enemy_group.add(enemy)
+    
+    
+    collisions()
+
+
 
 #THE GAME LOOP
-while True:
-    CLOCK.tick(60)
+while game_active:
+    CLOCK.tick(30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -148,9 +169,10 @@ while True:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bullet = Bullet(player.player_rect)
+                bullet = Bullet(player.rect)
                 bullets.append(bullet)
 
     draw_window()
 
     pygame.display.update()
+
