a2a2d18fdaf7dd6c335c37672f0b6dfc178d4538
Riwokib <riwellkib@gmail.com>
2024-02-09 11:53:36 +0300

    spritecollide: player/enemy collisions and groupcollide: enemy/bullet collisions

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -19,6 +19,9 @@ BUTTON_TEXT_FONT = pygame.font.Font('freesansbold.ttf',18)
 
 #WORLD
 BG = pygame.image.load("spaceinvaders/bg.jpg").convert()
+BULLET = pygame.transform.scale(pygame.image.load('spaceinvaders/bullet.png'),(20,25)).convert_alpha()
+ENEMY = pygame.image.load("spaceinvaders/enemy.png").convert_alpha()
+PLAYER = pygame.image.load('spaceinvaders/invader.png').convert_alpha()
 
 #SCORE VALUE
 score_value = 0
@@ -27,13 +30,13 @@ score_rect = score.get_rect(bottomleft = (0,600))
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self,):
+    def __init__(self, player_image):
         super().__init__()
-        self.PLAYER = pygame.image.load('spaceinvaders/invader.png').convert_alpha()
-        self.rect = self.PLAYER.get_rect(midbottom = (400,570))
+        self.image = player_image
+        self.rect = self.image.get_rect(midbottom = (400,570))
         self.player_velocity = 10
 
-    def move(self):
+    def update(self):
         pressed_keys = pygame.key.get_pressed()
 
         if pressed_keys[pygame.K_UP] and self.rect.top > 0:
@@ -46,20 +49,19 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.player_velocity
 
     def display(self, surface):
-        surface.blit(self.PLAYER, self.rect)
-
+        surface.blit(self.image, self.rect)
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, enemy_image):
         super().__init__()
-        self.ENEMY = pygame.image.load("spaceinvaders/enemy.png").convert_alpha()
+        self.image = enemy_image
         self.random_x_position = random.randint(0, SCREEN_WIDTH)
         #random_y_position = random.randint(-40, 0)
-        self.rect = self.ENEMY.get_rect(midtop = (self.random_x_position,0))
+        self.rect = self.image.get_rect(midtop = (self.random_x_position,0))
         self.enemy_speed = 0.01
         self.enemy_angle = 0
 
-    def move(self):
+    def update(self):
         self.rect.x += math.sin(self.enemy_angle) * self.enemy_speed
         self.rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
 
@@ -74,31 +76,34 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.y -= math.cos(self.enemy_angle) * self.enemy_speed
 
     def display(self, surface):
-        surface.blit(self.ENEMY, self.rect)
-
+        surface.blit(self.image, self.rect)
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, player_rect):
+    def __init__(self, bullet_image, player_rect):
         super().__init__()
-        self.BULLET = pygame.image.load('spaceinvaders/bullet.png').convert_alpha()
+        self.image = bullet_image
         bullet_x = player_rect.x + (player_rect.width // 2)
         bullet_y = player_rect.y + (player_rect.height // 2)
-        self.rect = self.BULLET.get_rect(midbottom =(bullet_x + 5, bullet_y))
+        self.rect = self.image.get_rect(midbottom =(bullet_x + 5, bullet_y))
         self.bullet_velocity = 15
 
-    def fire_bullet(self):
+
+    def update(self):
         self.rect.y -= self.bullet_velocity
 
     def display(self, surface):
-        surface.blit(pygame.transform.scale(self.BULLET, (20,25)), self.rect)
+        surface.blit(self.image, self.rect)
 
 
 #CREATE THE PLAYER
-player = Player()
+player = Player(PLAYER)
 
-#WAIT FOR ENEMIES & BULLETS
+#WAIT FOR enemies/bullets/collisions
 bullets = []
 enemies = []
+enemy_collided= []
+bullet_collided = []
+enemy_bullet_collided = []
 
 #ADD A USER EVENT to randomize enemy spawning
 SPAWN_ENEMY = pygame.USEREVENT + 1
@@ -108,20 +113,7 @@ pygame.time.set_timer(SPAWN_ENEMY, 2000)
 #SPRITE GROUPS for collisions
 enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
-
-
-def collisions():
-    '''handle collitions between player & enemy
-        enemy & bullet'''
-    
-    if pygame.sprite.spritecollide(player, enemy_group, True):
-        print('enemy collision!!')
-    
-    for bullet in bullets:
-        if pygame.sprite.spritecollide(bullet, enemy_group, True):
-            print('enemy shot')
-            enemy_group.empty()
-
+player_group = pygame.sprite.GroupSingle(player)
 
 def draw_window():
     #SHOW BACKGROUND
@@ -131,26 +123,17 @@ def draw_window():
     pygame.draw.rect(SCREEN, (255,182,193),score_rect)
     SCREEN.blit(score, score_rect)
 
-    #SHOW BULLETS
-    for bullet in bullets:
-        bullet.display(SCREEN)
-        bullet.fire_bullet()
-        bullet_group.add(bullet)
+    #SHOW BULLETS & MOVEMENTS
+    bullet_group.draw(SCREEN)
+    bullet_group.update()
+    
+    #SHOW ENEMIES & MOVEMENTS
+    enemy_group.update()    
+    enemy_group.draw(SCREEN)
     
     #SHOW & HANDLE PLAYER MOVEMENTS
     player.display(SCREEN)
-    player.move()
-
-    #SHOW ENEMIES & MOVEMENTS
-    for enemy in enemies:
-        enemy.display(SCREEN)
-        enemy.move()
-        enemy_group.add(enemy)
-    
-    
-    collisions()
-
-
+    player.update()
 
 #THE GAME LOOP
 while game_active:
@@ -162,17 +145,30 @@ while game_active:
             exit()
 
         if event.type == SPAWN_ENEMY:
-            enemy = Enemy()
+            enemy = Enemy(ENEMY)
             enemy.enemy_speed = random.randint(1,2)
             enemy.enemy_angle = random.uniform(0, math.pi * 2)
             enemies.append(enemy)
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bullet = Bullet(player.rect)
+                bullet = Bullet(BULLET, player.rect)
                 bullets.append(bullet)
 
+    #ADD BULLETS & ENEMIES to sprite groups
+    for bullet in bullets:
+        bullet_group.add(bullet)
+
+    for enemy in enemies:
+        enemy_group.add(enemy)
+
+
+    enemy_collided = pygame.sprite.spritecollide(player_group.sprite, enemy_group, True, pygame.sprite.collide_mask)
+
+    enemy_bullet_collided = pygame.sprite.groupcollide(bullet_group, enemy_group, True, True, pygame.sprite.collide_mask)
+
     draw_window()
+
 
     pygame.display.update()
 
